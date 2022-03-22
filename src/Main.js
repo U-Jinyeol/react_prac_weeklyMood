12bb5121e2f055bf881fa3dbc0e7d034c4f38dc7
U-Jinyeol <100745412+U-Jinyeol@users.noreply.github.com>
2022-03-22 17:52:04 +0900

    기능 완성본(주석처리전)

--- src/Main.js
+++ src/Main.js
@@ -2,136 +2,36 @@ import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 
 function Main(props) {
-  const weekly = ["월", "화", "수", "목", "금", "토", "일"];
   const history = useHistory();
-  const circleCount = [0,1,2,3,4];
-  const RandomGrade = () => {
-    return Math.ceil(Math.random() * 5)
-  };
-  const circleColor = () => {
-    let result =[];
-    
-    for (let i = 0; i < circleCount.length; i++) {
-      result.push(<Circle style={{
-        backgroundColor: "#ddd"
-      }}></Circle>);
-    } 
-    
-    for (let i = 0; i < RandomGrade(); i++) {
-      result.push(<Circle  style={{
-        backgroundColor: "#ffa07a"
-    }}></Circle>)
-  }return result
-}
+  const weekly = ["월", "화", "수", "목", "금", "토", "일"];
+  const circleCount = [0, 1, 2, 3, 4];
 
   return (
     <Wrap>
       <Title>내 일주일은?</Title>
-
-      <WeeklyGrade>
-        <Grade>
-          <h2>{weekly[0]}</h2>
-          {circleColor()}
-          <UpdateBtn
-            onClick={() => {
-              history.push(`/UpdateGrade/${weekly[0]}`);
-            }}
-            className="UpdateBtn"
-          />
-        </Grade>
-      </WeeklyGrade>
-
-      <WeeklyGrade>
-        <Grade>
-          <h2>{weekly[1]}</h2>
-          {circleCount.map((a, idx) => {
-            return <Circle />;
-          })}
-        </Grade>
-        <UpdateBtn
-          onClick={() => {
-            history.push(`/UpdateGrade/${weekly[1]}`);
-          }}
-          className="UpdateBtn"
-        />
-      </WeeklyGrade>
-
-      <WeeklyGrade>
-        <Grade>
-          <h2>{weekly[2]}</h2>
-          {circleCount.map((a, idx) => {
-            return <Circle style={{
-              backgroundColor: circleCount < RandomGrade ? "#ddd" : "#ffa07a"
-            }}/>;
-          })}
-        </Grade>
-        <UpdateBtn
-          onClick={() => {
-            history.push(`/UpdateGrade/${weekly[2]}`);
-          }}
-          className="UpdateBtn"
-        />
-      </WeeklyGrade>
-
-      <WeeklyGrade>
-        <Grade>
-          <h2>{weekly[3]}</h2>
-          {circleCount.map((a, idx) => {
-            return <Circle />;
-          })}
-          <UpdateBtn
-            onClick={() => {
-              history.push(`/UpdateGrade/${weekly[3]}`);
-            }}
-            className="UpdateBtn"
-          />
-        </Grade>
-      </WeeklyGrade>
-
-      <WeeklyGrade>
-        <Grade>
-          <h2>{weekly[4]}</h2>
-          {circleCount.map((a, idx) => {
-            return <Circle/>;
-          })}
-          <UpdateBtn
-            onClick={() => {
-              history.push(`/UpdateGrade/${weekly[4]}`);
-            }}
-            className="UpdateBtn"
-          />
-        </Grade>
-      </WeeklyGrade>
-
-      <WeeklyGrade>
-        <Grade>
-          <h2>{weekly[5]}</h2>
-          {circleCount.map((a, idx) => {
-            return <Circle />;
-          })}
-          <UpdateBtn
-            onClick={() => {
-              history.push(`/UpdateGrade/${weekly[5]}`);
-            }}
-            className="UpdateBtn"
-          />
-        </Grade>
-      </WeeklyGrade>
-
-      <WeeklyGrade>
-        <Grade>
-          <h2>{weekly[6]}</h2>
-          {circleCount.map((a, idx) => {
-            return <Circle />;
-          })}
-          <UpdateBtn
-            onClick={() => {
-              history.push(`/UpdateGrade/${weekly[6]}`);
-            }}
-            className="UpdateBtn"
-          />
-        </Grade>
-      </WeeklyGrade>
+      {weekly.map((weekly, index) => {
+        const Random = (n) => Math.ceil(Math.random() * n);
+        const RandomNum = Random(5);
+        return (
+          <Grade>
+            <h2>{weekly}</h2>
+            {circleCount.map((e, index) => {
+              return (
+                <Circle
+                  style={{
+                    background: index <= RandomNum ? "#ffa07a" : "#ddd",
+                  }}
+                />
+              );
+            })}
+            <UpdateBtn
+              onClick={() => {
+                history.push(`/UpdateGrade/${weekly}`);
+              }}
+            />
+          </Grade>
+        );
+      })}
     </Wrap>
   );
 }
@@ -150,13 +50,6 @@ const Wrap = styled.div`
 const Title = styled.h1`
   margin-bottom: 30px;
   font-size: 25px;
-`;
-
-const WeeklyGrade = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: black;
 `;
 
 const Grade = styled.div`
